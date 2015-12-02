a8bc8847602453b21bbbab69fcec75980dac6068
Ros Corcoran <roscorcoran@gmail.com>
2015-12-02 21:23:08 +0000

    Use react-router to show comments component

--- src/index.jsx
+++ src/index.jsx
@@ -15,12 +15,42 @@
 //13. Optional render methods like renderNavigation() or renderProfilePicture()
 //14. render
 
+//Libs
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Router, Route, Link, Redirect } from 'react-router'
+import { createHistory, useBasename } from 'history'
 
+//Components
 import CommentBox from './components/comments/CommentBox';
 
-ReactDOM.render(
-    <CommentBox />,
+//Constants
+const history = useBasename(createHistory)({
+  basename: '/home'
+});
+
+//App root elements
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <ul>
+            <li><Link to="/comments" activeClassName="active">Comments</Link></li>
+          </ul>
+          {this.props.children}
+        </div>
+    )
+  }
+}
+
+//Render via Router
+render((
+        <Router history={history}>
+          <Route path="/" component={App}>
+            <Route path="comments" component={CommentBox}>
+            </Route>
+          </Route>
+        </Router>
+    ),
     document.getElementById('app')
 );
